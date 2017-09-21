d292b06a67d93a4b81601655fe5d111636b8d649
jsanjak <jsanjak@uci.edu>
2017-09-21 18:57:36 -0400

    made histogram drop down

---
+++ app/utils.py
@@ -0,0 +1,59 @@
+from bokeh.plotting import figure
+from bokeh.embed import components
+from bokeh.models import Span, FactorRange
+from bokeh.charts import Bar
+
+import pandas as pd
+import numpy as np
+from math import pi
+
+def make_histogram(data,provider_id,provider_name,readmin_rate):
+		provider_data=data.ix[provider_id]
+		hist, edges = np.histogram(data, density=True, bins=50)
+		p = figure()#title = "Histogram of heart return days with value for "+ provider_name + " marked in red" )
+		p.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],
+        fill_color="#036564", line_color="#033649")
+		p.line([provider_data,provider_data],[0,.5],line_width=2,color="red")
+		p.yaxis.axis_label = None
+		p.xaxis.axis_label = readmin_rate
+		p.xaxis.axis_label_text_font_size = "20pt" 
+		p.yaxis.major_label_text_font_size = "10pt"
+		p.yaxis.axis_label_text_font_size = "20pt" 
+		return(p)
+
+def plot_lasso(lasso_results):
+	data = lasso_results.sort_values(by='rank_coef',ascending=True)
+	yrange = data['measure_names'].tolist()
+
+	p = figure(width=800, height=600, x_range =[0,1.1*np.max(data['rank_coef'].values)], y_range=yrange)
+
+	p.rect(x=data['rank_coef']/2, y=yrange, 
+		width=abs(data['rank_coef']), height=0.4,color=(76,114,176),
+    width_units="data", height_units="data")
+
+
+	#p = Bar(data, 
+	#	'measure_names', values='rank_coef', legend=False,
+	#	title="Most Actionable Factors")
+	#p.x_range = FactorRange(factors=data['measure_names'])
+	p.yaxis.major_label_orientation = pi/12
+	p.yaxis.axis_label = None
+	p.xaxis.axis_label = 'Actionability Index'
+	p.xaxis.axis_label_text_font_size = "20pt" 
+	p.yaxis.major_label_text_font_size = "10pt"
+	p.yaxis.axis_label_text_font_size = "20pt" 
+	return(p)
+
+def my_z_score(x):
+    z=((x.values - x.mean())/x.std())
+    return(z)
+
+
+def rank_direction(coef,rank):
+	if coef >0:
+		rank_coef = np.abs(coef)/(1 - rank)
+	else:
+		rank_coef = np.abs(coef)/(rank)
+	return(rank_coef)
+
+
--- app/views.py
+++ app/views.py
@@ -3,20 +3,24 @@ from app import app
 import pandas as pd
 import numpy as np
 from math import pi
-from textwrap import wrap
+#from textwrap import wrap
 from bokeh.plotting import figure
 from bokeh.embed import components
 from bokeh.models import Span, FactorRange
 from bokeh.charts import Bar
 from scipy import stats
 from sklearn.externals import joblib
-
+from app.utils import *
+from bokeh.io import output_file, show
+from bokeh.layouts import widgetbox
+from bokeh.models.widgets import Select
+#from utils import make_histogram, plot_lasso, my_z_score, rank_direction
 
 
 HF_env = joblib.load('HF_lasso_results.pkl') 
 #####
 #app = Flask(__name__)
-#app.secret_key = "super secret key"
+app.secret_key = "super secret key"
 
 
 ############Intro to app section
@@ -28,57 +32,28 @@ def returns_input():
 	return(render_template("returns_input.html",hospitals = sorted(HF_env['hospital_names'].values())))
 
 
-def plot_lasso(lasso_results):
-	data = lasso_results.sort_values(by='rank_coef',ascending=True)
-	yrange = data['measure_names'].tolist()
-
-	p = figure(width=800, height=600, x_range =[0,1.1*np.max(data['rank_coef'].values)], y_range=yrange)
-
-	p.rect(x=data['rank_coef']/2, y=yrange, 
-		width=abs(data['rank_coef']), height=0.4,color=(76,114,176),
-    width_units="data", height_units="data")
-
-
-	#p = Bar(data, 
-	#	'measure_names', values='rank_coef', legend=False,
-	#	title="Most Actionable Factors")
-	#p.x_range = FactorRange(factors=data['measure_names'])
-	p.yaxis.major_label_orientation = pi/12
-	p.yaxis.axis_label = None
-	p.xaxis.axis_label = 'Actionability Index'
-	p.xaxis.axis_label_text_font_size = "20pt" 
-	p.yaxis.major_label_text_font_size = "10pt"
-	p.yaxis.axis_label_text_font_size = "20pt" 
-	return(p)
-
-def my_z_score(x):
-    z=((x.values - x.mean())/x.std())
-    return(z)
-
-
-def rank_direction(coef,rank):
-	if coef <0:
-		rank_coef = (-1*coef)/(1 - rank)
-	else:
-		rank_coef = coef/rank
-	return(rank_coef)
-
 @app.route('/returns_predictors')
 def returns_predictors():
-	#readmin_rate=request.args.get('readmin_rate')
-	provider = HF_env['provider_id'][request.args.get('provider')]#session.get('provider',None)
+	#get the relevant information
+	current_feature_name = request.args.get("feature_name")
+	if current_feature_name == None:
+		current_feature_name = 'Staff responsiveness'#request.args.get("feature_name")
 
-	#SQLdata = MakeSQLData(readmin_rate).SQLdata
-	#SQLdata.rank(axis=0)/len(SQLdata)		
+	current_provider_name = session.get('provider',None)
+	if current_provider_name == None:
+		current_provider_name = request.args.get("provider")
+		session['provider'] = current_provider_name
+
+	provider = HF_env['provider_id'][current_provider_name]#	
 	rankmat = HF_env['model_mat'].rank(axis=0)/len(HF_env['model_mat'])
 
+	#get the lasso model results
 	lasso_results = HF_env['model_results'].loc[
 	HF_env['model_results']['coef'].values!=0].loc[
 	np.logical_not(
 	HF_env['model_results']['measure_id'].isin(
 	['H_RECMND_LINEAR_SCORE','MORT_30_COPD','MORT_30_HF']))]
 
-	#train_lasso(SQLdata)
 	lasso_results['ranks'] = [rankmat.loc[int(provider),item] for 
 	item in lasso_results['measure_id']]
 
@@ -87,20 +62,38 @@ def returns_predictors():
 
 	lasso_results['measure_names'] = [HF_env['measure_name_dict'][ID] for 
 	ID in lasso_results['measure_id']]
+	measure_id_dict = dict(zip(lasso_results['measure_names'],
+		lasso_results['measure_id']))
 
+	#make a select barm menu
+	menu = [i for i in lasso_results['measure_names']]
+	#select = Select(title="Indicator:",
+	# value='Median ED to admission time',
+	# options=menu)
+	
+	#drop_script, drop_div = components(widgetbox(select))
+
+	#bar plots
 	bar = plot_lasso(lasso_results)
 
+	#histograms
+	hist = make_histogram(HF_env['model_mat'].loc[:,measure_id_dict[current_feature_name]],int(provider),
+		HF_env['hospital_names'][int(provider)].title(),
+		str(current_feature_name)+ ' Score')
+	hist_script, hist_div = components(hist)
+
+
+	#script and div to send
 	improvement_script, improvement_div = components(bar)
-
-	return render_template('data.html',provider=HF_env['hospital_names'][int(provider)].title(),
-		readmin_rate='heart failure return days',
-		improvement_script=improvement_script, improvement_div=improvement_div)
-
-#if __name__ == '__main__':
-#  app.run(host='0.0.0.0',debug=True, port=8000)
-
-#hist = make_histogram(HF_env['model_mat'].iloc[:,0],provider,'Scaled heart failure return days')
-#hist_script, hist_div = components(hist)
+		
+	#['Staff responsiveness','Discharge information'],
+	return render_template('data.html',
+		provider_name=HF_env['hospital_names'][int(provider)].title(),
+		readmin_rate='heart failure return days', 
+		feature_names= menu, 
+		current_feature_name=current_feature_name,
+		improvement_script=improvement_script, improvement_div=improvement_div,
+		hist_script=hist_script, hist_div=hist_div)
 	
 
 
